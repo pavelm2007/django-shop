51bca9e51e461dca1bde9e199b1c5b308b833ef3
Alex Zaporozhets <alexzaporozhets@gmail.com>
2012-12-19 22:09:39 +0200

    Implemented Django sitemap framework for catalog models: Category + Product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import patterns, url
+from django.contrib import sitemaps
+from catalog.sitemap import ProductSitemap, CategorySitemap
+
 
 urlpatterns = patterns('catalog.views',
     url(r'^$', 'index'),
     url(r'^(?P<category_slug>[\w-]+)$', 'category'),
     url(r'^(?P<product_slug>[\w-]+).html$', 'product')
+)
+
+sitemaps = {
+    "categories": CategorySitemap,
+    "products": ProductSitemap
+}
+
+urlpatterns += patterns('',
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
 )
--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'mptt',
     'south',
     # Uncomment the next line to enable the admin:
