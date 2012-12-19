aa7692715df2e7117cdb45f4723bae78afff8f2f
Alex Zaporozhets <alexzaporozhets@gmail.com>
2012-12-19 21:02:03 +0200

    refactor media settings for DEBUG server

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,7 @@
 from django.conf.urls import patterns, url
-from django.conf import settings
 
 urlpatterns = patterns('catalog.views',
     url(r'^$', 'index'),
     url(r'^(?P<category_slug>[\w-]+)$', 'category'),
     url(r'^(?P<product_slug>[\w-]+).html$', 'product')
 )
-
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT}))
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,3 +8,9 @@ urlpatterns = patterns('',
     url(r'', include('catalog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT}))
