b3de83c583d7c04478075046e8fe7db94788069a
Alex Zaporozhets <alexzaporozhets@gmail.com>
2013-01-02 14:46:33 +0200

    На списке категорий для выбранных категорий доступны не все возможные действия

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,23 +6,31 @@ class OptionMPTTModelAdmin(MPTTModelAdmin):
     # specify pixel amount for this ModelAdmin only:
     mptt_level_indent = 20
 
-# Categories
-def hide_category(modelAdmin, request, queryset):
-    queryset.update(hidden=True)
-
-
-def un_hide_category(modelAdmin, request, queryset):
-    queryset.update(hidden=False)
-
-un_hide_category.short_description = "Mark selected stories as active"
-hide_category.short_description = "Mark selected stories as hidden"
 
 class CategoryMPTTModelAdmin(MPTTModelAdmin):
     # specify pixel amount for this ModelAdmin only:
     list_display = ('name', 'slug', 'is_active', 'count_products')
     mptt_level_indent = 20
-    actions = [hide_category, un_hide_category]
+    actions = ['hide_category', 'un_hide_category']
 
+    def hide_category(self, request, queryset):
+        rows_updated = queryset.update(hidden=True)
+        if rows_updated == 1:
+            message_bit = "1 category was"
+        else:
+            message_bit = "%s categories were" % rows_updated
+        self.message_user(request, "%s successfully marked as hidden." % message_bit)
+
+    def un_hide_category(self, request, queryset):
+        rows_updated = queryset.update(hidden=False)
+        if rows_updated == 1:
+            message_bit = "1 category was"
+        else:
+            message_bit = "%s categories were" % rows_updated
+        self.message_user(request, "%s successfully marked as active." % message_bit)
+
+    un_hide_category.short_description = "Mark selected as active"
+    hide_category.short_description = "Mark selected as hidden"
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'price')
