585cdca3764e8428525d1bfc15d57039fae41370
Alex Zaporozhets <alexzaporozhets@gmail.com>
2013-01-02 13:24:59 +0200

    На списке категорий для выбранных категорий доступны не все возможные действия

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,10 +6,22 @@ class OptionMPTTModelAdmin(MPTTModelAdmin):
     # specify pixel amount for this ModelAdmin only:
     mptt_level_indent = 20
 
+# Categories
+def hide_category(modelAdmin, request, queryset):
+    queryset.update(hidden=True)
+
+
+def un_hide_category(modelAdmin, request, queryset):
+    queryset.update(hidden=False)
+
+un_hide_category.short_description = "Mark selected stories as active"
+hide_category.short_description = "Mark selected stories as hidden"
+
 class CategoryMPTTModelAdmin(MPTTModelAdmin):
     # specify pixel amount for this ModelAdmin only:
     list_display = ('name', 'slug', 'is_active', 'count_products')
     mptt_level_indent = 20
+    actions = [hide_category, un_hide_category]
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -17,7 +29,6 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ['slug', 'price']
 
 admin.site.register(Product, ProductAdmin)
-
 
 admin.site.register(Category, CategoryMPTTModelAdmin)
 admin.site.register(Option, OptionMPTTModelAdmin)
