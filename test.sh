246ff391dfdf689f442703cca322fd406561c77b
Alex Zaporozhets <alexzaporozhets@gmail.com>
2013-02-02 18:06:10 +0200

    update script refactoring

--- test.sh
+++ test.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
-sudo -i
-sudo su www-data -c /bin/bash
-cd /data/projects/medtest
-source env/bin/activate
-cd django-shop
 
-# updating the codebase
-git checkout - . && git fetch && git pull --rebase
+update_site(){
+    cd /data/projects/medtest
+    source env/bin/activate
+    cd django-shop
 
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py syncdb
-python manage.py migrate
+    # updating the codebase
+    git checkout - . && git fetch && git pull --rebase
 
-exit
+    pip install -r requirements.txt
+    python manage.py collectstatic --noinput
+    python manage.py syncdb
+    python manage.py migrate
+
+}
+export -f update_site
+
+su www-data -c "bash -c update_site"
+
 service uwsgi restart
-
+service celeryd restart
+service nginx force-reload
 
 
 #python manage.py importyml /data/isells.eu/isells/scripts/medtest.xml --images
