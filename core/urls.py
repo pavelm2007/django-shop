10f0740692066d0d55f5de7bdbf2f137c1a63997
Alex Zaporozhets <alexzaporozhets@gmail.com>
2013-02-09 13:24:57 +0200

    #13 Fixed urls.py, /admin & /admin/ is allowed

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/?', include(admin.site.urls)),
     url(r'^imperavi/', include('imperavi.urls')),
     url(r'^cart/', include('cart.urls')),
     url(r'^compare/', include('compare.urls')),
