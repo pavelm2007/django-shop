1bfa6c6ecd6cf3c7c6f706236dbd3386ae6fffc7
Alex Zaporozhets <alexzaporozhets@gmail.com>
2013-02-09 15:44:39 +0200

    #13 Fixed urls.py, /admin & /admin/ is allowed

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/?', include(admin.site.urls)),
-    url(r'^imperavi/', include('imperavi.urls')),
-    url(r'^cart/', include('cart.urls')),
-    url(r'^compare/', include('compare.urls')),
-    url(r'^pricelist/', include('pricelist.urls')),
-    url(r'', include('catalog.urls')),
+                       url(r'^admin$', RedirectView.as_view(url='/admin/')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^imperavi/', include('imperavi.urls')),
+                       url(r'^cart/', include('cart.urls')),
+                       url(r'^compare/', include('compare.urls')),
+                       url(r'^pricelist/', include('pricelist.urls')),
+                       url(r'', include('catalog.urls')),
 )
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT}))
+                            (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.MEDIA_ROOT}))
